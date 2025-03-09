b0b7adf818f6c65325aa9c47b0fe896901bf293a
maxdetering <110093054+maxdetering@users.noreply.github.com>
2025-03-09 10:31:24 +0000

    Update zinspire.ts
    removed option noabstract

--- src/modules/zinspire.ts
+++ src/modules/zinspire.ts
@@ -65,13 +65,13 @@ export class ZInsMenu {
               _globalThis.inspire.updateSelectedItems("full")
             }
           },
-          {
-            tag: "menuitem",
-            label: getString("menuitem-submenulabel1"),
-            commandListener: (_ev) => {
-              _globalThis.inspire.updateSelectedItems("noabstract")
-            }
-          },
+          //{
+          //  tag: "menuitem",
+          //  label: getString("menuitem-submenulabel1"),
+          //  commandListener: (_ev) => {
+          //    _globalThis.inspire.updateSelectedItems("noabstract")
+          //  }
+          //},
           {
             tag: "menuitem",
             label: getString("menuitem-submenulabel2"),
@@ -109,13 +109,13 @@ export class ZInsMenu {
               _globalThis.inspire.updateSelectedCollection("full")
             }
           },
-          {
-            tag: "menuitem",
-            label: getString("menuitem-submenulabel1"),
-            commandListener: (_ev) => {
-              _globalThis.inspire.updateSelectedCollection("noabstract")
-            }
-          },
+          //{
+          //  tag: "menuitem",
+          //  label: getString("menuitem-submenulabel1"),
+          //  commandListener: (_ev) => {
+          //    _globalThis.inspire.updateSelectedCollection("noabstract")
+          //  }
+          //},
           {
             tag: "menuitem",
             label: getString("menuitem-submenulabel2"),
